a391076a74d758e388f10ccda2651610f0421520
Jon Bulica <jon.bulica@governikus.de>
2024-02-12 19:20:07 +0100

    fix setup for pypi

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     packages=find_packages(include=["fitconnect_client", "fitconnect_client.objects"]),
     version="0.1.0",
     description="A python SDK to interact with the German Fitconnect system",
+    long_description="A python SDK to interact with the German Fitconnect system",
     author="Jon Bulica",
     author_email="jonbulica99@gmail.com",
     install_requires=[
